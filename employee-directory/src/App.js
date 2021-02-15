7ff7473a061d87e090c6b8f5a590cb25f7832ff8
Jesal Mehta <jesaldmehta@gmail.com>
2021-02-15 18:46:28 -0500

    Created the page header for the Employee Directory page

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import './utils/API.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="main-container">
+    <div className="container-fluid">
+      <div className="jumbotron">
+       <h1 className="page-title">Employee Directory</h1>
+      </div>
     </div>
-  );
+   </div> 
+  )
 }
 
 export default App;
