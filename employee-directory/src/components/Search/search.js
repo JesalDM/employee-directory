3735a6d22a9a2b0ddb79e40398d1ec81e54a5ce8
Jesal Mehta <jesaldmehta@gmail.com>
2021-02-16 23:04:51 -0500

    Added state and handleInputChange function to the input element of the Search component

--- employee-directory/src/components/Search/search.js
+++ employee-directory/src/components/Search/search.js
@@ -2,12 +2,25 @@ import React from 'react';
 
 
 class Search extends React.Component{
+
+    state = {
+        search: ""
+    }
+
+    handleInputChange =(event)=>{
+       const name = event.target.name
+       const value = event.target.value
+        this.setState({
+           [name]:value
+        })
+    }
+ 
     render(){
         return(
             <div>
                 <nav className="navbar navbar-light bg-light">
                     <form className="form-inline">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search here" aria-label="Search" />
+                        <input className="form-control mr-sm-2" type="search" name="search" placeholder="Search here" value = {this.state.search} aria-label="Search" onChange={this.handleInputChange}/>
                         <button className="btn btn-success my-2 my-sm-0" type="submit">Search</button>
                     </form>
                 </nav>
