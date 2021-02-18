de32121d7b31e1d25c9ed1226aefe02a1d076062
Jesal Mehta <jesaldmehta@gmail.com>
2021-02-18 00:51:31 -0500

    Added code to format the name and date values dynamically in the table

--- employee-directory/src/components/Results.js
+++ employee-directory/src/components/Results.js
@@ -18,10 +18,10 @@ function Results(props) {
             {props.results.map(result => (
                 <tr key={result.email}>
                     <td><img src={result.picture.thumbnail} alt="profile-pic"/></td>
-                    <td>{result.name.first}{result.name.last}</td>
+                    <td>{result.name.first}{' '}{result.name.last}</td>
                     <td>{result.phone}</td>
                     <td>{result.email}</td>
-                    <td>{result.dob.date}</td>
+                    <td>{result.dob.date.replace(/T.*/, "")}</td>
                     <td>{result.nat}</td>
                 </tr>
             ))}
